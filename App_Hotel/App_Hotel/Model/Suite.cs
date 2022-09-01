382db2ca42265b39e94888560af0dd7c9de7d53d
HILARYSILVA <hilaryr646@gmail.com>
2022-09-01 15:55:45 -0300

    primeiro commit

---
+++ App_Hotel/App_Hotel/Model/Suite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App_Hotel.Model
+{
+    public class Suite : ContentPage
+    {
+           
+
+
+            public string  Descricao { get; set; }
+
+            public double Valordadiariaadulto { get; set; }
+
+            public double Valordadiariacrianca { get; set; }
+    }
+}
